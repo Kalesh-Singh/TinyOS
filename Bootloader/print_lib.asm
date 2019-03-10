a2f30ce11df820a3e870523572cbe74bc395fe98
Kalesh-Singh <kaleshsingh96@gmail.com>
2019-03-10 15:39:15 -0700

    An assembly routine that prints addresses as hexademicals

--- Bootloader/print_lib.asm
+++ Bootloader/print_lib.asm
@@ -37,3 +37,59 @@ print_string:
 
     popa            ; Restore original register values
     ret
+
+;
+; A function that prints an address as a hexadecimal.
+;
+; Expects the address to be passed in dx.
+;
+
+print_hex:
+    pusha               ; Push all registers to the stack
+    mov cx, 5           ; Index into the string
+
+    hex_loop:
+        mov ax, dx      ; Use ax as our working register
+        and ax, 0x000f  ; 0x1234 -> 0x0004 by masking the first 3 to 0
+        cmp ax, 10 
+        jge hex_letter  ; The char is a letter
+        add ax, 0x30    ; Convert to ASCII digit
+        jmp hex_loop_end
+
+        hex_letter:
+            add ax, 0x57    ; Convert to ASCII letter (A-F)
+
+        hex_loop_end:
+            ; Place the letter in ax (lower 8 bits) 
+            ; in the correct position in HEX_OUT
+            mov bx, HEX_OUT ; bx = string base address
+            add bx, cx      ; Address to place ASCII char
+            
+            mov [bx], al    ; Copy the ASCII char in ax to the address in bx
+                            ; NOTE: We do not place the entire ax (16 bits)
+                            ; into the address at bx as this would cause
+                            ; us to put the ASCII character followed by 
+                            ; a byte of zeros (the null character). 
+                            ; Remember Little Endianess ?
+                            ; And therefore the print_string function
+                            ; would return after printing the first 
+                            ; character in our hexadecimal number.
+                            ; Hence we used al (the lower 8 bits of ax)
+                            ; instead.
+
+            shr dx, 4      ; shift dx right by 4 bits
+            sub cx, 1      ; Decrement the index
+            cmp cx, 2       
+            jge hex_loop    ; Loop while index greater than or equal to 2
+
+    mov bx, HEX_OUT     ; print the string pointed to
+    call print_string   ; by BX
+    popa                ; Restore original register values
+    ret
+
+; Global variables
+HEX_OUT:
+    db '0x0000', 0
+
+
+
